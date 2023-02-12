a2cb452c82573f6222b04cdd61891b2f8e73c841
samr <samr.stuff@pm.me>
2023-02-12 09:23:59 -0800

    Added a second page

--- Go/WebServer.go
+++ Go/WebServer.go
@@ -9,14 +9,23 @@ import (
 )
 
 func greet(w http.ResponseWriter, r *http.Request) {
-	// This will output the text "Hello, World!" and the current time.
+	// This will output the text "Hello, World!" and the current time, as well as a link to /another.
 	fmt.Fprintf(w, "Hello, World! %s", time.Now())
+	fmt.Fprintf(w, "<a href='/another'>Another Page</a>")
+}
+
+func secondPage(w http.ResponseWriter, r *http.Request) {
+	// This is a second page for users to go to.
+	fmt.Fprintf(w, "This is another page!")
 }
 
 // This function starts the web server.
 func main() {
 	// If the user goes to /, return the value of greet().
 	http.HandleFunc("/", greet)
+	// If the user goes to /another, return the value of secondPage().
+	http.HandleFunc("/another", secondPage)
+
 	// Serve the server on port 8080.
 	http.ListenAndServe(":8080", nil)
 }
